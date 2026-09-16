8d511755adad6e459b5b103c4a5c30f942fbbfc7
Darren H <darrenhoang12@gmail.com>
2026-09-16 09:37:18 -0700

    fix import errors

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -5,3 +5,10 @@ interface ImportMetaEnv {
   readonly VITE_GOOGLE_MAPS_MAP_ID?: string;
   readonly VITE_API_BASE_URL?: string;
 }
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+
+  const component: DefineComponent;
+  export default component;
+}
